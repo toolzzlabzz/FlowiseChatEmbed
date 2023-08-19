bde0cfe368ec21b614f093289d436ba5c41d5f99
Felipe Feliciano <fehzl@live.com>
2023-08-18 21:44:38 -0300

    refactor: update z-index value to test action

--- src/features/bubble/components/Bubble.tsx
+++ src/features/bubble/components/Bubble.tsx
@@ -39,7 +39,7 @@ export const Bubble = (props: BubbleProps) => {
                     transform: isBotOpened() ? 'scale3d(1, 1, 1)' : 'scale3d(0, 0, 1)',
                     'box-shadow': 'rgb(0 0 0 / 16%) 0px 5px 40px',
                     'background-color': bubbleProps.theme?.chatWindow?.backgroundColor || '#ffffff',
-                    'z-index': 42424242
+                    'z-index': 42424245
                 }}
                 class={
                     `fixed sm:right-5 rounded-lg w-full sm:w-[400px] max-h-[704px]` +
