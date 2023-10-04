bd95eb06c984196df2fd73211a289c14b4f5a6b7
Felipe Feliciano <fehzl@live.com>
2023-10-04 09:32:15 -0300

    Revert

--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -31,7 +31,7 @@ export type BotProps = {
     fontSize?: number
 }
 
-const defaultWelcomeMessage = 'Hi there! How can I help?'
+const defaultWelcomeMessage = 'Olá, como posso te ajudar hoje?'
 
 /*const sourceDocuments = [
     {
